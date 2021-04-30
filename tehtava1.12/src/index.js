e7f07297ae29926cb9d49ccd46439d19550647a2
oliverlundstrom <65280277+oliverlundstrom@users.noreply.github.com>
2021-04-30 16:24:10 +0300

    Update index.js

--- tehtava1.12/src/index.js
+++ tehtava1.12/src/index.js
@@ -4,15 +4,33 @@ import ReactDOM from 'react-dom';
 const Display = (props) => {
   return(
   <div>
-    {props.anecdotes[0]}
+    {props.selected}
   </div>)
 }
 
-const Button = ({onClick}) => {
+const VoteDisplay = ({number}) => {
+  return(
+    <div>
+      has {number} votes
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => {
   return(
     <button onClick={onClick}>
-      next anecdote
+      {text}
     </button>
+  )
+}
+
+const BestAnecdote = ({points, anecdotes}) => {
+  const max_of_array = points.indexOf(Math.max(...points))
+  console.log(max_of_array)
+  return(
+    <div>
+      {anecdotes[max_of_array]}
+    </div>
   )
 }
 
@@ -25,22 +43,28 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ];
-   
-  const [selected, setSelected] = useState(0);
+    const [points, setPoints] = useState([0,0,0,0,0,0])
+    const copy = [...points]
+    const [selected, setSelected] = useState(0);
 
-  const suffleArray = () => {
-    for (let i = anecdotes.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random()*(i+1));
-      const temp = anecdotes[i];
-      anecdotes[i] = anecdotes[j];
-      anecdotes[j] = temp;
-    }
+  const randomize = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const addPoints = () => {
+    copy[selected] += 1
+    setPoints(copy)
   }
 
   return (
     <div>
-      <Display anecdotes={anecdotes}/>
-      <Button onClick={suffleArray}/>
+      <h1>Anecdote of the day</h1>
+      <Display selected={anecdotes[selected]}/>
+      <VoteDisplay number={points[selected]}/>
+      <Button onClick={addPoints} text="vote"/>
+      <Button onClick={randomize} text="next anecdote"/>
+      <h1>Anecdote with most votes</h1>
+      <BestAnecdote anecdotes={anecdotes} points={points}/>
     </div>
   )
 };
